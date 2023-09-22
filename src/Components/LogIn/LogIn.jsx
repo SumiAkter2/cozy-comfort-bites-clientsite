40d96dbc4fd1723443b89da380de8ecf3f61fd17
SumiAkter2 <akteralisha36@gmail.com>
2023-09-22 20:35:16 +0600

    added AuthProvider and firebase

--- src/Components/LogIn/LogIn.jsx
+++ src/Components/LogIn/LogIn.jsx
@@ -1,8 +1,33 @@
+import { useEffect, useRef, useState } from "react";
 import logInImg from "../../assets/LogIn/login.gif";
+import {
+  loadCaptchaEnginge,
+  LoadCanvasTemplate,
+  validateCaptcha,
+} from "react-simple-captcha";
+
 const LogIn = () => {
-    const handleSubmit = (e) => {
-        e.preventDefault();
+  const captchaRef = useRef(null);
+  const [disabled, setDisabled] = useState(true);
+  useEffect(() => {
+    loadCaptchaEnginge(6);
+  }, []);
+  const handleValidateCaptcha = () => {
+    const user_captcha_value = captchaRef.current.value;
+    console.log(user_captcha_value);
+    if (validateCaptcha(user_captcha_value) == true) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
     }
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
+  };
   return (
     <div>
       <div className="hero min-h-screen bg-base-200">
@@ -13,42 +38,63 @@ const LogIn = () => {
           <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 md:w-1/2">
             <div className="card-body">
               <h1 className="md:text-3xl font-bold text-center">Login now</h1>
-              <form className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input
-                  type="text"
-                  name="email"
-                  id="email"
-                  placeholder="email"
-                  className="input input-bordered"
-                />
+              <form onSubmit={handleSubmit}>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Email</span>
+                  </label>
+                  <input
+                    type="text"
+                    name="email"
+                    id="email"
+                    placeholder="email"
+                    className="input input-bordered"
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Password</span>
+                  </label>
+                  <input
+                    type="text"
+                    name="password"
+                    id="password"
+                    placeholder="password"
+                    className="input input-bordered"
+                  />
+                  <label className="label">
+                    <a href="#" className="label-text-alt link link-hover">
+                      Forgot password?
+                    </a>
+                  </label>
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <LoadCanvasTemplate />
+                  </label>
+                  <input
+                    type="text"
+                    name="captcha"
+                    ref={captchaRef}
+                    placeholder="Type the captcha above"
+                    className="input input-bordered"
+                  />
+                  <button
+                    onClick={handleValidateCaptcha}
+                    className="btn btn-outline btn-xs mt-2"
+                  >
+                    Validate
+                  </button>
+                </div>
+                <div className="form-control mt-6">
+                  <input
+                    disabled={disabled}
+                    className="btn btn-outline border-0 border-b-4"
+                    type="submit"
+                    value="Log In"
+                  />
+                </div>
               </form>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <input
-                  type="text"
-                  name="password"
-                  id="password"
-                  placeholder="password"
-                  className="input input-bordered"
-                />
-                <label className="label">
-                  <a href="#" className="label-text-alt link link-hover">
-                    Forgot password?
-                  </a>
-                </label>
-              </div>
-              <div className="form-control mt-6">
-                <input
-                  className="btn btn-outline border-0 border-b-4"
-                  type="submit"
-                  value="Log In"
-                />
-              </div>
             </div>
           </div>
         </div>
---
+++ src/Provider/AuthProvider.jsx
@@ -0,0 +1,50 @@
+import { createContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { app } from "../firebase/firebase.config";
+const AuthContext = createContext(null);
+const auth = getAuth(app);
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  console.log(authInfo);
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(email, password);
+  };
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+  const authInfo = {
+    user,
+    loading,
+    createUser,
+    signIn,
+    logOut,
+  };
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      console.log(currentUser);
+      setLoading(false);
+    });
+    return () => {
+      return unSubscribe();
+    };
+  }, []);
+
+  return <AuthContext.Provider>{children}</AuthContext.Provider>;
+};
+
+export default AuthProvider;
